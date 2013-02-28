f3f532f24053fc560b7d626d27107af614b07cb9
Robert Orzanna <orschiro@gmail.com>
2013-02-28 14:54:51 +0100

    Using CommandLineArguments instead of bat file to launch zotero with -profile argument

--- Zotero Standalone/ZoteroStandalonePortable/App/Zotero Standalone/StartZotero.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Zotero Standalone 
-echo If you start Zotero for the first time, this may take a while.
-echo Please wait...
-START zotero.exe -profile "..\..\Data\profile"
-ping -n 10 127.0.0.1 > NUL
-EXIT
